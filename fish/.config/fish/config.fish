750b0a767d716926bb8d919b4be937c233755589
René Koller <r.koller@me.com>
2023-03-21 20:32:17 +0100

    Disable default fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,14 @@
+# Commands to run in interactive sessions can go here
 if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
 
+# For Secretive
+set -x SSH_AUTH_SOCK /Users/renek/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+
+# Remove the default fish greeting
+set fish_greeting
 fish_add_path /Applications/Postgres.app/Contents/Versions/14/bin
 
-set -x SSH_AUTH_SOCK /Users/renek/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
+# Prompt
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
-
 starship init fish | source
