7ffb007c418ddce9240df0e8bb0b87741e6af42c
René Koller <r.koller@me.com>
2023-03-26 21:04:13 +0200

    Formatting

--- fish/.config/fish/conf.d/gcp.fish
+++ fish/.config/fish/conf.d/gcp.fish
@@ -2,11 +2,11 @@ abbr -a gcloud_removebg 'gcloud container clusters get-credentials removebg-app 
 abbr -a gcloud_danni 'gcloud container clusters get-credentials danni --no-user-output-enabled --zone europe-west4-b --project danni-278921 && gcloud config set project danni-278921 --no-user-output-enabled'
 
 function kl
-  echo "Listing running pod for workload '$argv'"
-  kubectl get pod --selector="app=$argv"
+    echo "Listing running pod for workload '$argv'"
+    kubectl get pod --selector="app=$argv"
 end
 
 function kc
-  echo "Connecting to running pod for workload '$argv'"
-  kubectl exec -it $(kubectl get pod --selector="app=$argv" --output jsonpath='{.items[0].metadata.name}') -- bash
+    echo "Connecting to running pod for workload '$argv'"
+    kubectl exec -it $(kubectl get pod --selector="app=$argv" --output jsonpath='{.items[0].metadata.name}') -- bash
 end
--- fish/.config/fish/conf.d/macos.fish
+++ fish/.config/fish/conf.d/macos.fish
@@ -2,28 +2,28 @@
 # Be sure to allow iTerm to modify apps
 # System Settings > Security & Privacy > App Management
 function set_icons
-  echo "Generating icons..."
+    echo "Generating icons..."
 
-  begin
-    sudo fish -c 'iconsur set /Applications/Notion.app'
-    sudo fish -c 'iconsur set /Applications/Spotify.app'
-    sudo fish -c 'iconsur set /Applications/Arc.app -i ~/Pictures/icons/arc.png -s 1.3'
-    sudo fish -c 'iconsur set /Applications/iTerm.app -i ~/Pictures/icons/terminal.icns -s 1.3'
-    killall Dock
-  end &> /dev/null
+    begin
+        sudo fish -c 'iconsur set /Applications/Notion.app'
+        sudo fish -c 'iconsur set /Applications/Spotify.app'
+        sudo fish -c 'iconsur set /Applications/Arc.app -i ~/Pictures/icons/arc.png -s 1.3'
+        sudo fish -c 'iconsur set /Applications/iTerm.app -i ~/Pictures/icons/terminal.icns -s 1.3'
+        killall Dock
+    end &>/dev/null
 
-  echo "Done"
+    echo Done
 end
 
 function restore_dock
-  echo "Restoring dock..."
+    echo "Restoring dock..."
 
-  set apps 'Arc' 'Fantastical' 'Mimestream' 'Slack' 'Notion' 'Spotify' 'Visual Studio Code' 'iTerm'
-  defaults write com.apple.dock persistent-apps -array
-  for app in $apps
-    defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'/Applications/$app.app'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-  end
-  killall Dock
+    set apps Arc Fantastical Mimestream Slack Notion Spotify 'Visual Studio Code' iTerm
+    defaults write com.apple.dock persistent-apps -array
+    for app in $apps
+        defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>'/Applications/$app.app'</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+    end
+    killall Dock
 
-  echo "Done"
+    echo Done
 end
